f4a99f61870f24ed2d6cb3d1d3742ebf6144b316
Philip Lapinski <41872747+PolskaTag@users.noreply.github.com>
2021-02-10 13:18:39 -0500

    Button + Functionality

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,10 +2,18 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Changes the textView on tap
+    fun onPress(view: View) {
+        val editText: TextView = findViewById(R.id.textView)
+        editText.text = getString(R.string.button_clicked)
     }
 }
